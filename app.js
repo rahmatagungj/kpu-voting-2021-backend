cd80be132fba7771e4116506fb4fe738affdca3a
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-07-29 15:28:48 +0700

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const DATABASE_NAME = "kpu-stkip-2021";
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const apiRoutes = require("./api-routes");
@@ -11,6 +12,14 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true});
 
